9e712e80320427ae62cf5b26a0e19ca3886c99ce
Daniel1603G <daniel.augusto.gutierrez@gmail.com>
2025-07-01 19:08:50 -0300

    spanw bien

--- Assets/Scripts/Spawning/CharacterSpawner.cs
+++ Assets/Scripts/Spawning/CharacterSpawner.cs
@@ -2,40 +2,37 @@ using UnityEngine;
 
 public class CharacterSpawner : MonoBehaviour, ISetupSpawner<CharacterSpawnConfig>
 {
+    [Header("Spawn Settings")]
     [SerializeField] private float spawnSpacingX = 10f;
     private int spawnIndex;
+
     private void OnEnable()
     {
         spawnIndex = 0;
     }
-public void Setup(CharacterSpawnConfig config)
-{
-    
-    Vector3 spawnPos = transform.position + new Vector3(spawnIndex * spawnSpacingX, 0f, 0f);
 
-    spawnIndex++;
-   
-    var instance = Instantiate(
-        config.characterPrefab,
-        transform.position,
-        transform.rotation
-    );
-    
- 
+    public void Setup(CharacterSpawnConfig config)
+    {
+        Vector3 spawnOffset = new Vector3(spawnIndex * spawnSpacingX, 0f, 0f);
+        Vector3 spawnPosition = transform.position + spawnOffset;
+        spawnIndex++;
 
-   
-    if (!instance.TryGetComponent<ISetup<CharacterModel>>(out var charSetup))
-        charSetup = instance.gameObject.AddComponent<Character>();
-    charSetup.Setup(config.characterModel);
+        var instance = Instantiate(
+            config.characterPrefab,
+            spawnPosition,
+            transform.rotation
+        );
 
-    
-    if (!instance.TryGetComponent<ISetup<IPlayerControllerModel>>(out var ctrlSetup))
-        ctrlSetup = instance.gameObject.AddComponent<PlayerController>();
-    ctrlSetup.Setup(config.controllerModel);
+        if (!instance.TryGetComponent<ISetup<CharacterModel>>(out var charSetup))
+            charSetup = instance.gameObject.AddComponent<Character>();
+        charSetup.Setup(config.characterModel);
 
-  
-    var animator = instance.GetComponentInChildren<Animator>()
-                   ?? instance.gameObject.AddComponent<Animator>();
-    animator.runtimeAnimatorController = config.animatorController;
-}
+        if (!instance.TryGetComponent<ISetup<IPlayerControllerModel>>(out var ctrlSetup))
+            ctrlSetup = instance.gameObject.AddComponent<PlayerController>();
+        ctrlSetup.Setup(config.controllerModel);
+
+        var animator = instance.GetComponentInChildren<Animator>()
+                       ?? instance.gameObject.AddComponent<Animator>();
+        animator.runtimeAnimatorController = config.animatorController;
+    }
 }
